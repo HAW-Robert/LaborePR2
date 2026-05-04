15f90b63e165b948c388a9a04c8a8b58cb5d513a
HAW-Robert <robertarnds1207gmail.com>
2026-05-04 09:47:30 +0200

    m

--- matrices/ColumnVector.java
+++ matrices/ColumnVector.java
@@ -1,5 +1,18 @@
 package matrices;
 
-public class ColumnVector {
+public class ColumnVector extends Matrix{
+    
+    public ColumnVector(int rows, int cols) {
+        super(rows, 1);
+        
+    }
+    public int get(){
+        //Durch verwendung von super. kann ich meine bereits existierende (und auf coefficients) zugreifende Methode aufrufen
+        return super.getRows();
+    }
+    public void set(int row,int col, double value){
+        super.set(row, 1, value);
+    }
+    
     
 }
