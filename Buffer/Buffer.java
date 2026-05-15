340abeb89c1628f4d7f8ebc97d2181f85498a2d3
HAW-Robert <robertarnds1207gmail.com>
2026-05-15 11:28:18 +0200

    Vorbereitung Praktikum 2

---
+++ Buffer/Buffer.java
@@ -0,0 +1,52 @@
+package Buffer;
+
+public class Buffer{
+    //attribute
+    private int size; //Wie viele Plätze sind gerade belegt?
+    private int first;// Index des ersten Objekts
+    private int last;//Index des letzten Objekts
+    private Object[] elements;// "elements" ist das Array vom typ Object welches unser Ringpuffer sein wird.
+
+    //Konstruktor
+public Buffer(int buffersize){
+    this.size = 0;
+    this.first = 0;
+    this.last = 0;
+this.elements = new Object[buffersize];
+}
+    //Methoden
+
+public Object get(){   //Das als erstes gekommene Object wird aus der Wartescvhlange entfernt.Ein Element ist im Ringpuffer nicht durch Löschen weg,sondern dadurch, dass first/size es nicht mehr einschließen.
+    Object leaver = elements[first];
+    if(first+1 >= elements.length){
+        this.first=0;
+    }else{
+        this.first+=1;
+    }
+    this.size-=1;
+return leaver;
+}
+public void put(Object object){     //Ein Objekt wird der Warteschlange hinzugefügt//  Gehe normalerweise einen Platz weiter.Wenn du am Ende angekommen bist springe wieder an den Anfang.
+    int m;
+    if(last+1 >= elements.length){// Prüfung ob last+1 das array sprengen würde 
+        m = 0; // Falls ja
+    }else{
+        m = last +1; // Falls nein
+    }
+    //1. A [0] 2. B [1] 3. C[2] unsw.
+    this.elements[m] = object;
+    this.last = m;
+    this.size += 1;
+}
+public int getSize(){
+    return size;
+}
+public int getBufferSize(){
+    return elements.length;
+}
+
+
+
+
+
+}
